41418b1d37b886682e3a6b07f7a3017ffd6104c4
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2025-10-24 16:00:27 +0200

    feat: imported i18n config

---
+++ .changeset/hot-ducks-change.md
@@ -0,0 +1,5 @@
+---
+'starlight-links': minor
+---
+
+Adds suport for imported Starlight i18n configuration ([`locales`](https://starlight.astro.build/reference/configuration/#locales)) object.
--- packages/client/src/libs/starlight.ts
+++ packages/client/src/libs/starlight.ts
@@ -6,14 +6,8 @@ import { FileType, Uri, workspace, type WorkspaceFolder } from 'vscode'
 import { getStarlightProjectFromConfig } from './ast'
 
 // https://github.com/withastro/astro/blob/6b92b3d455cb7b7ac09c5dcc0eceaabec1ba5903/packages/astro/src/core/config/config.ts#L27-L36
-const configFileNames = new Set([
-  'astro.config.mjs',
-  'astro.config.js',
-  'astro.config.ts',
-  'astro.config.mts',
-  'astro.config.cjs',
-  'astro.config.cts',
-])
+const configBaseName = 'astro.config'
+const fileExtensions = ['.mjs', '.js', '.ts', '.mts', '.cjs', '.cts']
 
 export async function getStarlightConfigFsPath(workspaceFolder: WorkspaceFolder, configDirectories: string[]) {
   const astroConfigUri = await getAstroConfigUri(workspaceFolder, configDirectories)
@@ -26,25 +20,25 @@ export async function getStarlightProject(fsPath: string) {
   const configData = await workspace.fs.readFile(Uri.file(fsPath))
   const configStr = new TextDecoder().decode(configData)
 
-  return getStarlightProjectFromConfig(configStr)
+  return getStarlightProjectFromConfig(configStr, async (relativeFsPath: string) => {
+    const rootDir = path.dirname(fsPath)
+    const fileDir = path.join(rootDir, path.dirname(relativeFsPath))
+    const fileBaseName = path.basename(relativeFsPath)
+
+    const fileUri = await findFileInDirectory(Uri.file(fileDir), fileBaseName)
+    if (!fileUri) throw new Error(`Failed to resolve file at relative path \`${relativeFsPath}\`.`)
+
+    const data = await workspace.fs.readFile(fileUri)
+    return new TextDecoder().decode(data)
+  })
 }
 
 async function getAstroConfigUri(workspaceFolder: WorkspaceFolder, configDirectories: string[]) {
   for (const configDirectory of configDirectories) {
     const uri = Uri.joinPath(workspaceFolder.uri, configDirectory)
 
-    try {
-      await workspace.fs.stat(uri)
-      const entries = await workspace.fs.readDirectory(uri)
-
-      for (const [name, type] of entries) {
-        if (type === FileType.File && configFileNames.has(name)) {
-          return Uri.joinPath(uri, name)
-        }
-      }
-    } catch {
-      // We can safely ignore errors related to missing directories.
-    }
+    const file = await findFileInDirectory(uri, configBaseName)
+    if (file) return file
   }
 
   return
@@ -56,4 +50,23 @@ export function getStarlightFsPaths(configFsPath: string, project: StarlightProj
     content: Uri.joinPath(Uri.file(path.dirname(configFsPath)), project.context.srcDir ?? 'src', 'content', 'docs')
       .fsPath,
   }
+}
+
+async function findFileInDirectory(directory: Uri, filename: string) {
+  try {
+    await workspace.fs.stat(directory)
+    const entries = await workspace.fs.readDirectory(directory)
+
+    for (const [name, type] of entries) {
+      if (type !== FileType.File) continue
+
+      for (const ext of fileExtensions) {
+        if (name === `${filename}${ext}`) return Uri.joinPath(directory, name)
+      }
+    }
+  } catch {
+    // We can safely ignore errors related to missing directories.
+  }
+
+  return
 }
